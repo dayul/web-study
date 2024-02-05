a15a472d9e5e95b669ed5eee493a2928dde7160b
dayul <dyulchoo@naver.com>
2024-02-05 22:57:36 +0900

    deleteTodo 함수 만들기

--- todo-list/JavaScript/main.js
+++ todo-list/JavaScript/main.js
@@ -1,12 +1,17 @@
 function addTodo() {
-    let Todo = $("#input-area").val();
-    console.log(Todo);
+    let todo = $("#input-area").val();
+    console.log(todo);
 
+    let id = Math.floor(Math.random() * 10);
     let item = `
-    <li class="list-group-item d-flex justify-content-between align-items-center" id="list-group-item">
-                  ${Todo}
-                  <span class="badge badge-primary badge-pill" onclick="deleteTodo()">완료</span>
+    <li class="list-group-item d-flex justify-content-between align-items-center list-${id}" id="list-group-item">
+                  ${todo}
+                  <span class="badge badge-primary badge-pill" onclick="deleteTodo(${id})">완료</span>
     </li>`;
     $("#list-group").append(item);     // ul 태그 안에 추가
     $("#input-area").val('');
+}
+
+function deleteTodo(id) {
+    $(`.list-${id}`).remove();
 }
